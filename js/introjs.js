877dff486bee945e78de87f2095f34779f21f00b
DongjuMun <dongjumun03@gmail.com>
2025-02-13 10:38:15 -0600

    Add introjs.js

--- js/introjs.js
+++ js/introjs.js
@@ -1,1 +1,12 @@
-// consola (log, )
+// consola (log, info, warn, error, assert, etc)
+console.log("Hola mundo wookie"); //no sale i
+console.info("Aquí andamos aprendiendo"); //sale i
+console.warn("Esta es una advertencia"); //warning
+console.error("aklfasga;sglkas"); //error
+// throw an error if the expression passed into the function is false.
+console.assert(1 == 1); // == solo compara el valor
+console.assert(1 === 1);  
+console.assert(1 == "1");  
+console.assert(1 === "1"); // === compara el valor y el tipo de dato
+console.assert(1 == true);
+console.assert(1 === true);    

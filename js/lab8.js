8073cdba1d4df02eb5d0f617975a8798041aa985
DongjuMun <dongjumun03@gmail.com>
2025-02-20 11:21:25 -0600

    Lab 8 completo con el servidor usando node.js

--- js/lab8.js
+++ js/lab8.js
@@ -32,3 +32,59 @@ let nums = [3, 2, 4, 1];
 target = 6;
 
 console.log(twoSum(nums, target));
+
+const http = require('http');
+
+const server = http.createServer( (request, response) => {
+    console.log(request.url);
+    response.setHeader('Content-type', 'text.html');
+    response.write(html);
+    response.end();
+})
+
+const html = `
+<!DOCTYPE html>
+<html data-theme="dark">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Hello Bulma!</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@1.0.2/css/bulma.min.css">
+  </head>
+  <body>
+  <section class="section"> 
+    <div class="container">
+      <h1 class="title">
+        Hello World
+      </h1>
+      <p class="subtitle">
+        My first website with <strong>Bulma</strong>!
+      </p>
+    </div>
+  </section>
+  <div class="columns">
+    <div class="column">
+      <button id="boton_imagen" class="button is-primary">
+        Mostrar imagen
+      </button>
+    </div>
+    <div class="column">
+      <span id="imagen_chewy"></span>
+    </div>
+    <div class="column">
+      <span id="imagen_grogu"></span>
+    </div>
+    <div class="column">
+      <span id="imagen_r2d2"></span>
+    </div>
+    <div class="column">
+      <span id="imagen_3po"></span>
+    </div>
+    <div class="column">6</div>
+  </div>
+  <script src="./js/introjs2.js"></script>
+  </body>
+</html>
+`;
+
+server.listen(3000);

f20c3e8ec43632a198266c5faa51fa38addb014f
DongjuMun <dongjumun03@gmail.com>
2025-02-20 11:26:53 -0600

    lab 8 Funcion de promedio | pair: Jorge

--- js/lab8.js
+++ js/lab8.js
@@ -1,4 +1,13 @@
+const calcularPromedio = (arreglo) => {
+    let suma = 0;
+    for (let item of arreglo) {
+        suma += item;
+    }
+    return suma / arreglo.length;
+};
 
+const arreglo = [1, 2, 3, 4, 5];
+console.log(calcularPromedio(arreglo))
 
 const escribirEnArchivo = (str) => {
     const file_s = require('fs');

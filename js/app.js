fe23df9131160b4de30c6edc0e0759900dc17167
DongjuMun <dongjumun03@gmail.com>
2025-02-20 10:36:08 -0600

    node response

--- js/app.js
+++ js/app.js
@@ -30,7 +30,10 @@ for (let item of arreglo) {
 const http = require('http');
 
 const server = http.createServer( (request, response) => {
-
+    console.log(request.url);
+    response.setHeader('Content-Type', 'text.html');
+    response.write(html);             
+    response.end();
 });
 
 //createServer(function, )
@@ -40,3 +43,48 @@ server.listen(3000);
 //127.0.0.1:3000/
 //o 
 //localhost:3000/
+
+const html = `
+<!DOCTYPE html>
+<html data-theme="dark">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Hello Bulma!</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@1.0.2/css/bulma.min.css">
+  </head>
+  <body>
+  <section class="section"> 
+    <div class="container">
+      <h1 class="title">
+        Hello World
+      </h1>
+      <p class="subtitle">
+        My first website with <strong>Bulma</strong>!
+      </p>
+    </div>
+  </section>
+  <div class="columns">
+    <div class="column">
+      <button id="boton_imagen" class="button is-primary">
+        Mostrar imagen
+      </button>
+    </div>
+    <div class="column">
+      <span id="imagen_chewy"></span>
+    </div>
+    <div class="column">
+      <span id="imagen_grogu"></span>
+    </div>
+    <div class="column">
+      <span id="imagen_r2d2"></span>
+    </div>
+    <div class="column">
+      <span id="imagen_3po"></span>
+    </div>
+    <div class="column">6</div>
+  </div>
+  <script src="./js/introjs2.js"></script>
+  </body>
+</html>
+`

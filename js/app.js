86a0ecf2ea6704d40669a621f919d5c632083a4a
DongjuMun <dongjumun03@gmail.com>
2025-02-20 10:21:17 -0600

    Crear servidor desde node

--- js/app.js
+++ js/app.js
@@ -10,7 +10,8 @@ file_system.writeFileSync('hola.txt', 'Hola desde node');
 
 setTimeout(() => {
     console.log("jojo te hackeé");
-}, 20000);
+}, 20000); 
+//Este comando corre al final ya que es una function asincronica
 
 const arreglo = [5000, 50, 230, 120, 13, 29, 329, 493, 921,1210];
 
@@ -25,3 +26,17 @@ for (let item of arreglo) {
 //setTimeout(function, la cantidad de milisegundos)
 
 //Manera asincronica
+
+const http = require('http');
+
+const server = http.createServer( (request, response) => {
+
+});
+
+//createServer(function, )
+
+server.listen(3000);
+//arriba de 3000 porque no se ocupan normalmente
+//127.0.0.1:3000/
+//o 
+//localhost:3000/

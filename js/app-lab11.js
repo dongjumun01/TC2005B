7673e9ef0c5ffdfbcbcd5faee35efecce2f12728
DongjuMun <dongjumun03@gmail.com>
2025-02-25 10:47:47 -0600

    get post body

--- js/app-lab11.js
+++ js/app-lab11.js
@@ -1,5 +1,11 @@
+const personajes = [];
+
 const express = require('express');
 const app = express();
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: false}));
+
 
 //Middleware
 app.use((request, response, next) => { //con use registramos middleware
@@ -13,8 +19,26 @@ app.use('/main', (request, response, next) => {
     response.send("Hola desde una ruta");
 });
 
-app.use('/personajes/agregar', (request, response, next) => {
+// cuando se registra un middleware con app.get(),
+// el middleware solo se registra para el metodo HTTP GET
+app.get('/personajes/agregar', (request, response, next) => {
     response.send(html_header + html_content_form + html_footer);
+});
+
+// cuando se registra un middleware con app.post(),
+// el middleware solo se registra para el metodo HTTP POST
+app.post('/personajes/agregar', (request, response, next) => {
+    console.log(request.body);
+    personajes.push(request.body.nombre);
+    console.log(personajes);
+    let html = html_header;
+    for (let personaje of personajes) {
+        html += html_card_header;
+        html += personaje;
+        html += html_card_footer;
+    }
+    html += html_footer;
+    response.send(html);
 });
 
 app.use((request, response, next) => {
@@ -26,52 +50,3 @@ app.use((request, response, next) => {
 
 app.listen(3000);
 
-const html_header = `
-<!DOCTYPE html>
-<html data-theme="dark">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Hello Bulma!</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@1.0.2/css/bulma.min.css">
-  </head>
-  <body>
-  <section class="section"> 
-    <div class="container">
-      <h1 class="title">
-        Hello There!
-      </h1>
-    </div>
-</section>
-  `;
-
-const html_content_form = `
-    <div class="columns">
-    <div class="column">
-        <form action="/main" method="POST">
-        <label class="label">Brain storming</label>
-        <input class="input is-primary" type="text" placeholder="Idea" id="idea" name="idea"/>
-        <br><br>
-        <input type="submit" id="boton_imagen" class="button is-primary" value="Guardar idea"/>
-        </form>
-    </div>
-    <div class="column">
-        <span id="imagen_chewy"></span>
-    </div>
-    <div class="column">
-        <span id="imagen_grogu"></span>
-    </div>
-    <div class="column">
-        <span id="imagen_r2d2"></span>
-    </div>
-    <div class="column">
-        <span id="imagen_3po"></span>
-    </div>
-    <div class="column">6</div>
-    </div> `;
-const html_footer = `
-    
-  <script src="./js/introjs2.js"></script>
-  </body>
-</html>
-`;

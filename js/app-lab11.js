bec62ab246e725fd09b84ce3079e217348d6d92c
DongjuMun <dongjumun03@gmail.com>
2025-02-25 10:23:35 -0600

    Middleware para una ruta especifica

--- js/app-lab11.js
+++ js/app-lab11.js
@@ -7,9 +7,17 @@ app.use((request, response, next) => { //con use registramos middleware
     next(); //Le permite a la peticion avanzar hacia el siguiente middleware
 });
 
+//si muevo esto al final, no va a ejecutar 
+// ya que no tiene next el middleware al final
+app.use('/main', (request, response, next) => { 
+    response.send("Hola desde una ruta");
+});
+
 app.use((request, response, next) => {
     console.log('Otro Middleware!');
     response.send('Hola mundo!'); //Manda respuesta
 });
+
+
 
 app.listen(3000);

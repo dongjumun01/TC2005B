c9a2f6f191c010128a62a81a464fc022f313843c
DongjuMun <dongjumun03@gmail.com>
2025-02-27 10:14:39 -0600

    Utilizar ejs

--- js/app-lab11.js
+++ js/app-lab11.js
@@ -2,8 +2,11 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
+app.set('view engine', 'ejs');
+app.set('view', 'view');
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: false}));

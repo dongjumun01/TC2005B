289645502ef7531ad051c9155840fbe5fdfcaf39
DongjuMun <dongjumun03@gmail.com>
2025-03-04 09:39:19 -0600

    instalar y configurar express-session

--- js/app-lab11.js
+++ js/app-lab11.js
@@ -3,41 +3,27 @@ const app = express();
 const path = require('path');
 
 
+
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
+const session = require('express-session');
+app.use(session({
+    secret: 'mi string secreto que debe ser un string aleatorio muy largo, no como éste', 
+    resave: false, //La sesión no se guardará en cada petición, sino sólo se guardará si algo cambió 
+    saveUninitialized: false, //Asegura que no se guarde una sesión para una petición que no lo necesita
+}));
+
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: false}));
-
-
-// //Middleware
-// app.use((request, response, next) => { //con use registramos middleware
-//     console.log('Middleware!');
-//     next(); //Le permite a la peticion avanzar hacia el siguiente middleware
-// });
-
-//si muevo esto al final, no va a ejecutar 
-// ya que no tiene next el middleware al final
-// app.use('/main', (request, response, next) => { 
-//     response.send("Hola desde una ruta");
-// });
 
 const rutasUsuarios = require('../routes/user.routes.js');
 app.use('/users', rutasUsuarios);
 
 const rutasPersonajes = require('../routes/personajes.routes.js');
-
 app.use('/personajes', rutasPersonajes);
-
-// const rutasProductos = require('../routes/productos.routes.js');
-// app.use('/productos', rutasProductos);
-
-// app.use((request, response, next) => {
-//     console.log('Otro Middleware!');
-//     response.send('Hola mundo!'); //Manda respuesta
-// })
 
 app.use((request, response, next) => {
     response.status(404).send('Página no encontrada');
